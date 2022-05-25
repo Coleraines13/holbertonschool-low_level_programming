0d1d942d30c149ce6f1cbe61956548d787307fb0
root <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>
2022-05-25 15:20:08 -0700

    else if why c 24

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,15 +10,16 @@
 int main(void)
 {
 	int n;
-	char *positive = "positive\n";
-	char *negative = "negative\n";
-	char *zero = "zero\n";
+	int t;
+	char *positive = "and is greater than 5\n";
+	char *negative = "and is less than 8 and not 0\n";
+	char *zero = "and is 0\n";
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	t = n % 10;
 
-	printf("%d is %s", n, n == 0 ? zero : n > 0 ? positive : negative);
+	printf("Last digit of %d is %d", n, t);
 	printf("%s", t == 0 ? zero : t > 5 ? greaterfive : lesssix);
 	
 	return (0);
