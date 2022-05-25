7d488ae64a980a5de4dfb5a76e6e369b32315b5b
root <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>
2022-05-25 14:29:38 -0700

    else if why c 15

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,12 +10,12 @@
 int main(void)
 {
 	int n;
-	char *positive = "posotive\n";
+	char *positive = "positive\n";
 	char *negative = "negative\n";
 	char *zero = "zero\n";
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("%d is %s", n, n == 0 ? zero : n > 0 ? positive : negative);
-	return(0);
+	return (0);
 }
