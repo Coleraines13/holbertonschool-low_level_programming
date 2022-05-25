38f64b9057f63f8cf10607f4837ba1d8d7c67228
root <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>
2022-05-25 15:02:53 -0700

    else if why 21

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,12 +14,10 @@ int main(void)
 	char *greaterfive = "and is greater than 5\n";
 	char *lesssix = "and is less than 6 and not 0\n";
 	char *zero = "and is 0\n";
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	t = n % 10;
 	printf("Last digit of %d is %d ", n, t);
 	printf("%s", t == 0 ? zero : t > 5 ? greaterfive : lesssix);
-
 	return (0);
 }
