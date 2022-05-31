043a47781164402ec823b8bcf385b29cd1f23a9d
root <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>
2022-05-31 12:11:20 -0700

    debugging 2

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+		int n;
+		int t;
+		char *positive = "and is greater than 5\n";
+		char *negative = "and is less than 8 and not 0\n";
+		char *zero = "and is 0\n";
+
+		srand(time(0));
+		n = rand() - RAND_MAX / 2;	
+		t = n % 10;
+
+		printf("Last digit of %d is %d", n, t);
+	        printf("%s", t == 0 ? zero : t > 5 ? greaterfive : lesssix);
+												
+		return (0);
+}
