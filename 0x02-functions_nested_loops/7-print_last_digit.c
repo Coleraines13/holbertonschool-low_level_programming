c3cb565086e50851f561060a61feea7a2dcada33
root <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>
2022-06-02 12:28:40 -0700

    nested loops 23

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: number to be checked
+ *
+ * Return: 0
+ */
+int print_last_digit(int n)
+{
+	int i = n % 10;
+
+	if (i < 0)
+	{
+		i = 0 - i;
+	}
+	_putchar(i = '0');
+	return (i);
+}
