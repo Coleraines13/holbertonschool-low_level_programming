4bcd3a09a711b312915cbbcc71a9440aca603b44
root <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>
2022-06-02 13:45:47 -0700

    nested loops 35

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds 2 integers
+ * @a: first integer input
+ * @b: second integer input
+ *
+ * Return: results of addition
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
