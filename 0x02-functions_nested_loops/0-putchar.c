5c01c7efa603b71d3db0d3240777875c870354c5
root <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>
2022-06-02 08:50:48 -0700

    nested loops 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * prints 0 using _putchar
+ * Return: Always 0 (Success)
+ */
+
+int _putchar(char 0)
+{
+	_putchar('0');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * using _putchar to print 0
+ * Return: on (Success) 1
+ */
+
+int _putchar('0');
+int _putchar('\n');
