9449bcdda4fcb9ab4ee72c9e0306738916288ef1
root <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>
2022-06-03 13:38:04 -0700

    arrays pointers and strings 32

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,16 +9,16 @@
 
 void print_rev(char *s)
 {
-	int i = 0;
-	int length;
-	
-	for (length = 0; s[lenght] != '\0'; length++)
-	{
-	}
+		int i = 0;
+			int length;
 
-	for (i = lenght - 1; i >= 0; i--)
-	{
-		_putchar(s[i]);
-	}
-	_putchar('\n');
+				for (length = 0; s[length] != '\0'; length++)
+						{
+								}
+
+					for (i = length - 1; i >= 0; i--)
+							{
+										_putchar(s[i]);
+											}
+						_putchar('\n');
 }
