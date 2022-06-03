d9ba0f16aa95088b5b0830be6b12f5b43beaa2d5
root <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>
2022-06-03 15:27:46 -0700

    pointers arrays and strings 44

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,13 +5,13 @@
  * puts2 - this function prints every other character of a string
  * (Divisible by two)
  *@str: pointer, the string to print
- *     
+ *    
  * Return: void
  */
 void puts2(char *str)
 {
 	int i = 0;
-			
+
 	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
