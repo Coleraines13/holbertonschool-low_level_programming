066b8f6897cb764bbb097c538ed0829e6d5ef630
root <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>
2022-06-03 16:20:56 -0700

    debugging 41

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,27 @@
-#include "main.h"
+nclude <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
 /**
- *
- *
- * main - tests function prints if integer positive or negative
- * Return: 0
- */
-
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-	int i;
+	int n;
+	int t;
+	char *positive = "and is greater than 5\n";
+	char *negative = "and is less than 8 and not 0\n";
+	char *zero = "and is 0\n";
 
-	i = 0;
-	positive_or_negative(i);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	t = n % 10;
+
+	printf("Last digit of %d is %d", n, t);
+	printf("%s", t == 0 ? zero : t > 5 ? greaterfive : lesssix);
+												
 
 	return (0);
 }
