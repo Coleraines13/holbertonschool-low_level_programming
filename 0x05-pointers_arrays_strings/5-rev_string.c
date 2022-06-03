cbd753f5e6478080b4524adf17e63ea7013b8b32
root <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>
2022-06-02 20:41:08 -0700

    arrays pointers strings 20

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,7 +2,7 @@
 
 /**
  * rev_string - reverse a string.
- * 
+ *
  *    *@s: the string to be reversed.
  *
  */
@@ -14,18 +14,17 @@ void rev_string(char *s)
 
 	for (length = 0; s[length] != '\0'; length++)
 	{
-		}
+	}
 
 	j = length - 1;
 	i = 0;
-
 	while (j > i)
 	{
-			v1 = s[i];
-			v2 = s[j];
-			s[i] = v2;
-			s[j] = v1;
-			j--;
-			i++;
-		}
+	v1 = s[i];
+	v2 = s[j];
+	s[i] = v2;
+	s[j] = v1;
+	j--;
+	i++;
+	}
 }
