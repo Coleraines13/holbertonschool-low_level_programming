a789142d80624677ccc4e98221d2354033e88f88
root <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>
2022-06-02 19:30:29 -0700

    arrays 4

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * swap_int - swap the value of the two integers
+ * @a: first integer
+ * @b: second integer
+ */
+
+void swap_int(int *a, *b)
+{
+	int c;
+
+	c = 0;
+	c = *a;
+	*a = *b;
+	*b =*c;
+}
