61e675af7c5810edfcab743259b98a5ec3bfe1cc
root <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>
2022-06-03 18:10:06 -0700

    nested loops 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *  * more_numbers - prints numbers from 0 to 14 10 times
- *   * 
- *    * Return: void
- *     */
+ * more_numbers - prints numbers from 0 to 14 10 times
+ *
+ * Return: void
+ */
 
 void more_numbers(void)
 {
