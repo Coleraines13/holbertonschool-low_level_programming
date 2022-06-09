5f7041b506f3bb6c3a2faf14a514d853383865b8
root <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>
2022-06-09 12:28:20 -0700

    strings pointer 20

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,22 +10,12 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int a = 0, b = 0;
+	int i;
 
-	while (src[b])
-	{
-		b++;
-	}
-	while (a < n && src[a])
-	{
-		dest[a] = src[a];
-		a++;
-	}
-
-	while (a < n)
-	{
-		dest[a] = '\0';
-		a++;
-	}
+	for (i = 0; src[i] != '\0' && i < n; i++)
+		dest[i] = src[i];
+	while ( i < n)
+		dest[i++] = '\0';
 	return (dest);
 }
+
