6934b5c51a4d99e31c79938ce45317d6accf9105
root <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>
2022-06-08 18:46:51 -0700

    recursions

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen_recursion - return stringlength
+ * @s: input string
+ * Return: void
+ */
+int _strlen_recursion(char *s)
+{
+	if (* == '\0')
+		return (0);
+	return (_strlen_recursion(++s) = 1);
+}
