a62e09b5982f0b618a025d7aa04c6c45c76135c4
root <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>
2022-06-09 20:32:34 -0700

    recursion 25

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,17 +1,24 @@
 #include "main.h"
 /**
  * _pow_recursion - finds x to the power of y
- * @x: base
- * @y: exponent
- * Return: result of x to the power of y
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
+ * Return: on success 1
  */
-int _put_recursion(int x, int y)
+int sqrt2(int a, int b)
 {
-	if (y < 0)
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
 		return (-1);
-	if (y == 0)
-		return (x >= 0 ? 1 : -1);
-	if (y == 1)
-		return (x);
-	return (x * _put_recursion(x, y - 1) * (x < 0 ? -1 : 1));
+	return (sqrt2(a, b + 1));
+}
+/**
+ * _sqrt_recursion - return the natural square root of n
+ * @n: number integer
+ * return: on success 1
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt2(n, 1));
 }
