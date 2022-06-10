0360275834f980e8c29b86f38bfcb44782fce770
root <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>
2022-06-09 17:46:19 -0700

    recursion 16

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,7 +2,7 @@
 /**
  * _print_rev_recursion - prints string in reverse
  * @s: input string
- * Return: 
+ * Return: on success 1
  */
 void _print_rev_recursion(char *s)
 {
@@ -14,4 +14,3 @@ void _print_rev_recursion(char *s)
 	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }
-	
