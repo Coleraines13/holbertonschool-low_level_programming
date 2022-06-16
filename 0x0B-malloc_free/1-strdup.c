b1de48a32d90af2684f9c14c4f949594bd924ffd
root <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>
2022-06-16 11:38:21 -0700

    malloc 13

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,15 +3,15 @@
 #include <stdlib.h>
 
 /**
- * _strdup - this returns a pointer to a newly allocated space
- * in memory which contains a copy of the string given as a parameter
+ * _strdup - function that returns a pointer to a newly allocated space
+ * in memory, which contains a copy of the string given as a parameter
  *
  * @str: string of chars
  *
  * Return: address of the newly allocated memory
  */
 
-char *_strdup(char *str)
+char *_str(char *str)
 {
 	unsigned int len;
 	unsigned int i, j;
@@ -30,11 +30,12 @@ char *_strdup(char *str)
 	str_copy = malloc(len * sizeof(char) + 1);
 	if (str_copy == NULL)
 		return (NULL);
+
 	j = 0;
 	while (j < len)
 	{
 		str_copy[j] = str[j];
-			j++;
+		j++;
 	}
 	str_copy[j] = '\0';
 	return (str_copy);
