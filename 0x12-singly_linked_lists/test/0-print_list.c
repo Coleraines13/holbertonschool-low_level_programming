a5b672acd04b19932b071872e11fb7953bedbce7
root <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>
2022-07-08 12:55:59 -0700

    fixed betty for 0-print

--- 0x12-singly_linked_lists/test/0-print_list.c
+++ 0x12-singly_linked_lists/test/0-print_list.c
@@ -8,15 +8,13 @@
 size_t print_list(const list_t *h)
 {
 	size_t i = 0;
-
 	while (h)
-
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
-
+		
 		h = h->next;
 		i++;
 	}
