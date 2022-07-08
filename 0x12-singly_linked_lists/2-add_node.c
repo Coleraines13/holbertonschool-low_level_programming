47f59c6631bc87e36fc5410014922074af780b49
root <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>
2022-07-08 12:39:54 -0700

    singly linked 3

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_node - prints list
+ * @head: pointer
+ * @str: string pointer
+ * Return: pointer
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	unsigned int len + 0;
+	list_t *newnode;
+	while (str[len])
+		len++;
+	newnode = malloc(sizeof(list_t));
+	if (!newnode)
+	{
+		return (NULL);
+	}
+
+	newmode->str = strdup(str);
+	newnode->len = len;
+	newnode->next = (*head);
+	(*head) = newnode;
+	return (*head);
+}
