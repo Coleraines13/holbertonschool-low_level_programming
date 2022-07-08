83c27806fce5118a8e63aa4a909a6ad2ac6bf75c
root <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>
2022-07-08 13:24:25 -0700

    remade 0-print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,25 +1,22 @@
 #include "lists.h"
 
 /**
- * print_list - prints n count n list
+ * print_list - prints n coubnt n list
  * @h: pointer to first
  * Return: pointer
  */
+
 size_t print_list(const list_t *h)
 {
 	size_t i = 0;
-
 	while (h)
-
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
+		if (!h->str)
+			printf("[0 (nil\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
-
 		h = h->next;
 		i++;
 	}
-
 	return (i);
 }
