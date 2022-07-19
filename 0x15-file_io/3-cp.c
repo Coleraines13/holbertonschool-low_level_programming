68faa889437af1e18ca19cccc422f52aa09ba2e8
root <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>
2022-07-19 10:28:42 -0700

    betty

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 	fileFrom = open(argv[1], O_RDONLY);
 	if (fileFrom == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: cant read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 	exit(98);
 	}
 	fileTo = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
