a8a707517f90ab0f5f4d6c06f94c2e1623c09379
root <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>
2022-08-05 05:51:29 -0700

    5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hashtable
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t **head;
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+	if (ht->array == NULL)
+		return;
+	head = ht->array;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (head[i] != NULL)
+			printf("'%s': '%s', ", head[i]->key, head[i]->value);
+	}
+	printf("}\n");
+}
