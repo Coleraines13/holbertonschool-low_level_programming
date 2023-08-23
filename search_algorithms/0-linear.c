951e84ba4997bc3bfd07faf1fa5a115fdcbb8e12
Coleraines13 <nttreaper@gmail.com>
2023-08-23 11:23:29 -0700

    fixed i think

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 
-int linear_search(int *array, size_t size, int value) {
-    if (array == NULL) {
+/**
+ * linear_search - Search for a value in an array using linear search.
+ * @array: Pointer to the first element of the array.
+ * @size: Number of elements in the array.
+ * @value: The value to search for.
+ *
+ * Return: Index of the value if found, otherwise -1.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+    if (array == NULL)
         return -1;  // Return -1 for a NULL array
-    }
 
-    for (size_t i = 0; i < size; ++i) {
+    for (size_t i = 0; i < size; ++i)
+    {
         printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value) {
+        if (array[i] == value)
             return (int)i;  // Return the index where value is located
-        }
     }
 
     return -1;  // Return -1 if value is not found
--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,31 +1,39 @@
 #include <stdio.h>
 
-int binary_search(int *array, size_t size, int value) {
-    if (array == NULL) {
+/**
+ * binary_search - Search for a value in a sorted array using binary search.
+ * @array: Pointer to the first element of the array.
+ * @size: Number of elements in the array.
+ * @value: The value to search for.
+ *
+ * Return: Index of the value if found, otherwise -1.
+ */
+int binary_search(int *array, size_t size, int value)
+{
+    if (array == NULL)
         return -1;  // Return -1 for a NULL array
-    }
 
     int left = 0;
     int right = (int)size - 1;
 
-    while (left <= right) {
+    while (left <= right)
+    {
         int mid = (left + right) / 2;
         printf("Searching in array: ");
-        for (int i = left; i <= right; ++i) {
+        for (int i = left; i <= right; ++i)
+        {
             printf("%d", array[i]);
-            if (i != right) {
+            if (i != right)
                 printf(", ");
-            }
         }
         printf("\n");
 
-        if (array[mid] == value) {
+        if (array[mid] == value)
             return mid;  // Return the index where value is located
-        } else if (array[mid] < value) {
+        else if (array[mid] < value)
             left = mid + 1;
-        } else {
+        else
             right = mid - 1;
-        }
     }
 
     return -1;  // Return -1 if value is not found
