84a80230377f3d560657290515ab991ae063b03c
Coleraines13 <nttreaper@gmail.com>
2023-08-23 11:48:45 -0700

    fixed

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -10,31 +10,44 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-    if (array == NULL)
-        return -1;  // Return -1 for a NULL array
+    int left, right, mid, i;
 
-    int left = 0;
-    int right = (int)size - 1;
+    if (array == NULL)
+    {
+        return (-1);  /* Return -1 for a NULL array */
+    }
+
+    left = 0;
+    right = (int)size - 1;
 
     while (left <= right)
     {
-        int mid = (left + right) / 2;
+        mid = (left + right) / 2;
         printf("Searching in array: ");
-        for (int i = left; i <= right; ++i)
+        
+        for (i = left; i <= right; ++i)
         {
             printf("%d", array[i]);
             if (i != right)
+            {
                 printf(", ");
+            }
         }
         printf("\n");
 
         if (array[mid] == value)
-            return mid;  // Return the index where value is located
+        {
+            return mid;  /* Return the index where value is located */
+        }
         else if (array[mid] < value)
+        {
             left = mid + 1;
+        }
         else
+        {
             right = mid - 1;
+        }
     }
 
-    return -1;  // Return -1 if value is not found
+    return (-1);  /* Return -1 if value is not found */
 }
--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -10,6 +10,7 @@
 #include <fcntl.h>
 #include <stddef.h>
 
+int binary_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
